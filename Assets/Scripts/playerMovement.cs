c84ab4de108196cc55aab8ac082394279c80f097
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-20 19:20:14 +0000

    Initial commit

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 500f;
+    public float sidewaysForce = 500f;
+
+	// Update is called once per frame
+    // Use FixedUpdate when using physics
+	void FixedUpdate () {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce*Time.deltaTime); // x, y, z
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -3f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
